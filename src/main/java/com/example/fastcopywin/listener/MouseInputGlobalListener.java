d1519591c754019737bcecc900d70271349d3293
SundayLi <lijinyu@sensorsdata.cn>
2022-07-28 01:30:33 +0800

    - 优化无用代码

--- src/main/java/com/example/fastcopywin/listener/MouseInputGlobalListener.java
+++ src/main/java/com/example/fastcopywin/listener/MouseInputGlobalListener.java
@@ -3,9 +3,7 @@ package com.example.fastcopywin.listener;
 import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.mouse.NativeMouseEvent;
 import com.github.kwhat.jnativehook.mouse.NativeMouseInputListener;
-import org.apache.commons.lang3.tuple.Pair;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,10 +14,7 @@ public class MouseInputGlobalListener implements NativeMouseInputListener {
 
   private static MouseInputGlobalListener mouseInputGlobalListener;
 
-
   private Map<String, List<ListenerOperation>> listenersMap;
-
-  private volatile Point point;
 
   private MouseInputGlobalListener() {
     // 添加监听项
@@ -36,20 +31,11 @@ public class MouseInputGlobalListener implements NativeMouseInputListener {
   }
 
   @Override
-  public void nativeMouseMoved(NativeMouseEvent e) {
-    this.point = e.getPoint();
-  }
-
-  @Override
   public void nativeMouseReleased(NativeMouseEvent e) {
     Optional.ofNullable(listenersMap.get(String.valueOf(e.getButton()))).ifPresent(list -> list.forEach(ListenerOperation::operation));
   }
 
   public void registerCustomMouseKeyAfterReturnEvent(int code, ListenerOperation listenerOperation) {
     listenersMap.computeIfAbsent(String.valueOf(code), k -> new ArrayList<>()).add(listenerOperation);
-  }
-
-  public Point getPoint() {
-    return point == null ? new Point(0, 0) : point;
   }
 }
